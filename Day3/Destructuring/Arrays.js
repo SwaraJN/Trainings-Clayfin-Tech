2996f20ce11be6c2410812b95ff3866a498b52c3
SwaraJN <75193540+SwaraJN@users.noreply.github.com>
2022-06-12 23:59:17 +0530

    first commit

---
+++ Day3/Destructuring/Arrays.js
@@ -0,0 +1,13 @@
+var names=['sam','ram','joe','nick','rock']
+
+var [a,b,c,d,e,f]=names; //assigned tonew variables
+var [sam,ram,joe,nick,rock]=names; //assigned to existing variables
+console.log(a);
+console.log(b);//and so on ....
+//OR
+console.log(sam,ram,joe);//etc...
+
+//Array Destructuring Means Nothing but to break down a structure into simple parts 
+//we can destructure an Array , Object , & maps
+//we set them into new variables or existing variables
+//Can use a Default value
---
+++ Day3/Destructuring/Objects.js
@@ -0,0 +1,9 @@
+var data ={
+    fname:'swaraj',
+    lname:'nikam',
+    dob:'default'
+}
+    var {fname,lname,dob}=data
+    console.log(fname,lname,dob)
+    
+    
